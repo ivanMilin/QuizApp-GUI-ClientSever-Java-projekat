b43bda18bb5b8d527ca2065ea503e2b6eb9cc97d
Ivan Milin <101069339+ivanMilin@users.noreply.github.com>
2024-04-21 04:16:33 +0200

    Add files via upload

---
+++ My_Application/dist/javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"Login"},{"l":"my_application"}];updateSearchResults();
---
+++ My_Application/src/my_application/My_Application.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package my_application;
+
+/**
+ *
+ * @author Ivan Milin
+ */
+public class My_Application {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
